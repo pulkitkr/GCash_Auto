1dc3ce6dc8513f19b6a7176b1399d8492341f2fe
pulkitkr <pulkit.kumar@igsindia.net>
2022-09-28 14:50:31 +0530

    add

--- src/test/java/com/GCash_GGivesScripts/GCASHScripts.java
+++ src/test/java/com/GCash_GGivesScripts/GCASHScripts.java
@@ -14,7 +14,7 @@ import com.utility.Utilities;
 public class GCASHScripts {
 
 	private com.business.gCASH.GCASHBusinessLogic GCASHBusiness;
-
+//Hello
 	@BeforeTest
 	public void Before() throws InterruptedException {
 		GCASHBusiness = new com.business.gCASH.GCASHBusinessLogic("gcash");
